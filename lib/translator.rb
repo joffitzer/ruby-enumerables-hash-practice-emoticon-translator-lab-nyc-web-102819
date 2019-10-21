78a9c2e032136ff9f0b57a785f74443c018b0820
Jonah Offitzer <joffitzer@gmail.com>
2019-10-21 20:33:16 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -25,4 +25,5 @@ end
 def get_english_meaning(emoticon_file, emoticon)
   emoticon_lib = load_library(emoticon_file)
   english_meaning = emoticon_lib['get_meaning'][emoticon]
+  english_meaning ? english_meaning : 'Sorry, that '
 end
