f6c39f4fa46396e5beed1c416c424302594a92f9
Jonah Offitzer <joffitzer@gmail.com>
2019-10-21 20:32:38 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -24,5 +24,5 @@ end
 
 def get_english_meaning(emoticon_file, emoticon)
   emoticon_lib = load_library(emoticon_file)
-  english_meaning = 
+  english_meaning = emoticon_lib['get_meaning'][emoticon]
 end
